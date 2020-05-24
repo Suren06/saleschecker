bfdd6b9d972cd7f4084037641ecbfd7b6d66af11
Suren <suren@Surens-MBP.realtek>
2020-05-24 12:54:05 +0800

    coomit

--- src/main/java/com/saleschecker/SalescheckerApplication.java
+++ src/main/java/com/saleschecker/SalescheckerApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SalescheckerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SalescheckerApplication.class, args);
 	}
