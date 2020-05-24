93b4c169f133a0ad21bec7cb190cdc80f961ef15
Suren <suren@Surens-MBP.realtek>
2020-05-24 11:31:59 +0800

    first coomit to saleschecker project

---
+++ src/main/java/com/saleschecker/SalescheckerApplication.java
@@ -0,0 +1,13 @@
+package com.saleschecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalescheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalescheckerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saleschecker/SalescheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saleschecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalescheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
